a7acf0ec934de809998a6d963f184bc849b7ea60
wassertim <tim@digitalband.ru>
2016-01-31 13:00:02 +0100

    add pages

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,9 +1,42 @@
-import {Component} from 'angular2/core';
 import {HighlightDirective} from './highlight.directive';
+import {Component, DynamicComponentLoader, OnInit, Injector} from "angular2/core";
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/components/app.component.html',
     directives: [HighlightDirective]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  loader: DynamicComponentLoader;
+  injector: Injector;
+  pages = [
+    {
+      name: 'Basic Types',
+      url: 'app/pages/basic-types.html'
+    }
+  ];
+
+  constructor(dcl: DynamicComponentLoader, injector: Injector) {
+    this.loader = dcl;
+    this.injector = injector;
+  }
+
+  ngOnInit() {
+    window.fetch(this.pages[0].url).then((response) => {
+      return response.text();
+    }).then((body) => {
+      this.loader.loadAsRoot(
+        this.toComponent(body, [HighlightDirective]),
+        '#main',
+        this.injector
+      );
+    });
+  }
+
+  toComponent(template, directives = []) {
+    @Component({ selector: 'fake-component', template, directives })
+    class FakeComponent {}
+
+    return FakeComponent;
+  }
+}
